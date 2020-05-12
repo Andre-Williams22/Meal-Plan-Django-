933107bf17991cbe52d7c80a64dbff373a8fb95e
Andre Williams <awjrs22@gmail.com>
2020-05-12 14:34:19 -0700

    allows users to add their own profile pictures

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Profile(models.Model):
+    # create 1 to 1 relationship for each user and profile
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+    def __str__(self):
+        return f" {self.user.username} Profile"
+
+
+
+# python manage.py makemigrations 
+# python manage.py migrate 
+
+# python manage.py shell 
+# user = User.objects.filter(username='andre').first() 
+# user
+# user.profile
+# user.profile.image
+# user.profile.url
