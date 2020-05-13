5ede25ec1bc651b1c7bd1b89a5883550a86e5dbc
Andre Williams <awjrs22@gmail.com>
2020-05-12 19:55:22 -0700

    updates User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +11,20 @@ class UserRegisterForm(UserCreationForm):
         # specify model we want to interact with 
         model = User 
         fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        # specify model we want to interact with 
+        model = User 
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        # specify model we want to interact with 
+        model = Profile 
+        fields = ['image']
